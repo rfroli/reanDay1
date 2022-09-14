d75e6a679b0056bb2c64a4920acda58d808a3990
rfroli <rfroli@gmail.com>
2022-09-14 10:20:02 +0200

    phase 1

---
+++ lab.js
@@ -0,0 +1,9 @@
+let nom = "Reginald";
+let formation = "REAN";
+
+function info()
+{
+    return "Formation chez PLB";
+}
+let phrase = `Je suis ${nom}, la formation est : ${formation}, la formation se déroule chez ${info()}`;
+console.log(phrase);
--- labs.js
+++
@@ -1,1 +0,0 @@
-console.log("Bienvenue à REAN")
